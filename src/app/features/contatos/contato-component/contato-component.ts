704469f0fcbab37cfa54ab9d192c10941bbfcc16
marcio-cavalcante <marcio_acc@hotmail.com>
2025-07-24 18:10:22 -0300

    Criado o compoente cadastro com a tela inicial do sistema

---
+++ src/app/features/contatos/contato-component/contato-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato-component',
+  standalone: false,
+  templateUrl: './contato-component.html',
+  styleUrl: './contato-component.scss'
+})
+export class ContatoComponent {
+
+}
--- src/app/features/contatos/contatos-module.ts
+++ src/app/features/contatos/contatos-module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContatosRoutingModule } from './contatos-routing-module';
+import { ContatoComponent } from './contato-component/contato-component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContatoComponent
+  ],
   imports: [
     CommonModule,
     ContatosRoutingModule
+  ],
+  exports: [
+    ContatoComponent
   ]
 })
 export class ContatosModule { }
