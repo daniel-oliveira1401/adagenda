2018b2594fa643c833fcdf9447161db5fa6ba499
danielmweb <ldanielmoreno-2014@outlook.com>
2025-07-25 08:31:52 -0300

    implementado servicos de persistencia

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,8 +1,11 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { InjectionToken, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ContatoPersistence } from './shared/interface/contato-persistence';
+
+export const CONTATO_PERSISTENCE_IMPLEMENTATION = new InjectionToken<ContatoPersistence>('contato_persistence_token');
 
 @NgModule({
   declarations: [
---
+++ src/app/shared/models/contato-model.ts
@@ -0,0 +1,8 @@
+export class ContatoModel {
+    constructor(
+        public id : string,
+        public nome : string,
+        public telefone : string,
+        public email : string
+    ){}
+}
